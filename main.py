7c16bc22bcebd1778721c13341b3e20eff7c3606
Branislava Sandrih <branislava.sandrih@gmail.com>
2018-02-08 09:39:27 +0100

    fix

--- main.py
+++ main.py
@@ -5,50 +5,43 @@ import sys
 from features_extraction.dataset import Dataset
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
-from sklearn.feature_selection import SelectFromModel
-from sklearn.feature_selection import SelectKBest, f_classif, chi2, f_regression
 import numpy as np
 from sklearn.metrics import classification_report, confusion_matrix
 import matplotlib.pyplot as plt
-
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import tree
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn import cross_validation, metrics   #Additional scklearn functions
-from sklearn.grid_search import GridSearchCV   #Perforing grid search
-from sklearn.gaussian_process.kernels import RBF
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
 from sklearn.utils import shuffle
-
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
 import pandas as pd
-import time
 
 
 if __name__ == '__main__':
     
     if len(sys.argv) < 2:
-        print('Usage: python main.py path_to_xml_file')
+        print('Usage: python main.py path_to_csv')
         exit(1)
 
+    '''
     # create dataset frame
     dataset = Dataset(filename=sys.argv[1], verbose=True)
-    
     X, y = dataset.data, dataset.target
+    '''
+    
+    # import data
+    df = pd.read_csv(sys.argv[1])
+    
+    # shuffle dataset
+    df = shuffle(df)
+
+    # samples and labels
+    y = np.array(df['class'])
+    X = np.array(df.ix[:, df.columns != 'class'])
+    print(np.shape(X), np.shape(y))
 
     # train/test sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+    print(np.shape(X_train), np.shape(y_train), np.shape(X_test), np.shape(y_test))
     
-    clf = LinearSVC(C=1, penalty='l1', dual=False)
+    clf = LinearSVC(C=0.1, penalty='l2')
     # fitting model
     clf.fit(X_train, y_train)
     # predicting labels
